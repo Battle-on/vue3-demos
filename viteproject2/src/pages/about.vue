77862985dd3baf915631459254fae5dfe75fda5e
wangjunfa <466564750@qq.com>
2022-06-05 21:01:17 +0800

    feat: css var & vue

--- viteproject2/src/pages/about.vue
+++ viteproject2/src/pages/about.vue
@@ -1,4 +1,34 @@
 
 <template>
-    <h1>这是关于页面</h1>
+  <div>
+      about
+    <h1 @click="add">{{ count }}</h1>
+
+     <h1 @click="add">{{ obj.color1 }}</h1>
+  </div>
 </template>
+
+<script setup>
+import { ref, reactive } from "vue";
+let count = ref(1)
+let color = ref('red')
+
+let obj = reactive({
+    color1: '111'
+})
+
+const arr = ['red', 'blue', '#f99', '#f00', '#f90', 'green', 'yellow', '#963', '#987', '#678']
+function add() {
+  count.value++
+  const index = Math.floor(Math.random() * 10)
+  obj.color1 = arr[index]
+  color.value = arr[index]
+}
+</script>
+
+<style scoped>
+h1 {
+  color:v-bind(color);
+  cursor: pointer;
+}
+</style>>
